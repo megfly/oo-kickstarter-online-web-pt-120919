db807c96f121b334dbc34855b5915fdda4085c3f
Meghan Flynn <mflynn333@gmail.com>
2020-01-22 01:37:04 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,11 @@
 class Backer 
   
-  attr_reader :name, :backed_projects
+  attr_reader :name, :backed_projects, :title
   
   def initialize(name)
     @name = name 
     @backed_projects = []
+    @title = title
   end 
   
 end 
